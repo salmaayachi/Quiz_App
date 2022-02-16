a95c0251b73ce6346aa7f93e589f5ce8d5c92a1d
selma <salma.ayachi@insat.u-carthage.tn>
2022-02-16 12:19:01 +0100

    9bal l ftour

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import './question.dart';
 
 void main() {
-  runApp(const MyCoolApp());
+  runApp(MyCoolApp());
 }
 
-class MyCoolApp extends StatelessWidget {
-  const MyCoolApp({Key? key}) : super(key: key);
+class MyCoolApp extends StatefulWidget {
+  @override
+  State<MyCoolApp> createState() => _MyCoolAppState();
+}
+
+class _MyCoolAppState extends State<MyCoolApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,18 +35,22 @@ class MyCoolApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            const Text('the question!'),
-            RaisedButton(
-              child: Text("Answer1"),
-              onPressed: null,
+            Question(
+              questions.elementAt(_questionIndex),
             ),
             RaisedButton(
-              child: Text("Answer2"),
-              onPressed: null,
+              child: const Text("Answer1"),
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
-              child: Text("Answer3"),
-              onPressed: null,
+              child: const Text("Answer2"),
+              onPressed: () {
+                print("answer 2 chosen ");
+              },
+            ),
+            RaisedButton(
+              child: const Text("Answer3"),
+              onPressed: () => print('Answer 3 chosen'),
             ),
           ],
         ),
