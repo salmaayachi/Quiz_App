d26cbb54489d0804ade30b8a8e10552e7259a941
fedi <fadikossentini@insat.u-carthage.tn>
2022-02-17 16:44:38 +0100

    flutter basics

--- lib/result.dart
+++ lib/result.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  final VoidCallback resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    var resultText = 'You did it!';
+    if (resultScore <= 8) {
+      resultText = "You are awesome and innocent!";
+    } else if (resultScore <= 12) {
+      resultText = "Pretty likeable";
+    } else if (resultScore <= 16) {
+      resultText = "you are strange";
+    } else {
+      resultText = 'You are bad';
+    }
+
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('You did it ! '),
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 36,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          TextButton(
+            child: Text('Restart Quiz!'),
+            style: ButtonStyle(
+                foregroundColor: MaterialStateProperty.all(Colors.red)),
+            onPressed: resetHandler,
+          ),
+        ],
+      ),
     );
   }
 }
